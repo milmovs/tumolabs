7b4fdb8ed8ce2396a49cca85e1de4eb36a984b6e
Milena <45044461+milmovs@users.noreply.github.com>
2022-11-29 21:12:37 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+enter = (input("Insert time to countdown (h:m:s): ").split(":"))
+h = int(enter[0])
+m = int(enter[1])
+s = int(enter[2])
+def countdown(h,m,s):
+  t = (h*3600) + (m*60) + (s)
+  while t >= 0:
+    print("%02d:%02d:%02d" % (t / 3600, (t / 60) % 60, t % 60))
+    time.sleep(1)
+    t -= 1
+countdown(h,m,s)
